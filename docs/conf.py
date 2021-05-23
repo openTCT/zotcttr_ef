fd5eea6b7b6b24403814140c2ffbe5150a7ba178
egemenyilmaz <egemeny1053@gmail.com>
2021-05-23 06:35:08 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+autodoc_mock_imports = ['myst_parser']
 
 # -- Project information -----------------------------------------------------
 
